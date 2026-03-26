d28e81731ff804bf86e4aa1443f640c59e19a711
Bryer <cybermakeroficial@gmail.com>
2026-03-26 19:11:52 -0300

    fixing repo*

--- obj/x64/Debug/GambaBank.AssemblyInfo.cs
+++ obj/x64/Debug/GambaBank.AssemblyInfo.cs
@@ -12,12 +12,12 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("GambaBank")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1be2cb91930d314ac1937fe4559e2ac613de55c4")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.1+2779444beddce3d5be7920600968e64a61860c90")]
 [assembly: System.Reflection.AssemblyProductAttribute("GambaBank")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GambaBank")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/goatcorp/SamplePlugin.git")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.1.0")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/seventity7/GambaBank.git")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
 
